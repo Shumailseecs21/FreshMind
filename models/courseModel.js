958b446c34ee6e8a082b0147003e21e4b2ff539c
Shumailseecs21 <sqaiser.bese21seecs@seecs.edu.pk>
2023-12-30 12:46:07 +0500

    models checking

---
+++ models/courseModel.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const {SchemaTypes} = require("mongoose");
+const Schema=mongoose.Schema;
+
+const courseSchema=new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    courseContent:[{
+        courseContentId:{
+            type:Schema.Types.ObjectId,
+            ref:"CourseContent",
+            required:true
+        }
+    }]
+});
+
+module.exports=mongoose.model("Course",courseSchema);
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,17 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
+const certificationSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    }
+});
+
 const userSchema=new Schema({
     username:{
         type:String,
@@ -14,7 +25,30 @@ const userSchema=new Schema({
         type:String,
         required:true,
     },
-    // courses:
+    role: {
+        type: String,
+        enum: ['admin', 'doctor', 'member'],
+        required: true
+    },
+    courses: [
+        {
+            docSessionId: {
+                type: Schema.Types.ObjectId,
+                ref: 'Course',
+                required: true
+            }
+        }
+    ],
+    docSessions: [
+        {
+            docSessionId:{
+                type: Schema.Types.ObjectId,
+                ref: 'DoctorSession',
+                required:true
+            }
+        }
+    ],
+    certifications:certificationSchema
 });
 
 module.exports=mongoose.model("User",userSchema);
